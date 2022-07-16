ab446ef3ff1a15b9d7fa3cc124db477f756764cc
Andres Camilo Chavez <81716319+AndresCamiloChavez@users.noreply.github.com>
2022-07-16 17:28:05 -0500

    first commit

---
+++ lib/pages/pagina1_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Pagina1Page  extends StatelessWidget {
+   
+  const Pagina1Page({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+         child: Text('Pagina1Page'),
+      ),
+    );
+  }
+}
---
+++ lib/pages/pagina2_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Pagina2Page extends StatelessWidget {
+   
+  const Pagina2Page({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+         child: Text('Pagina2Page'),
+      ),
+    );
+  }
+}
