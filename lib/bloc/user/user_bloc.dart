e0001fbf04e2a94f307a8cff389c24f9c79b4fe3
Andres Camilo Chavez <81716319+AndresCamiloChavez@users.noreply.github.com>
2022-07-18 21:08:27 -0500

    arreglos para notas en udemy bloc

--- lib/bloc/user/user_bloc.dart
+++ lib/bloc/user/user_bloc.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:meta/meta.dart';
-
 import '../../models/usuario.dart';
-
+import 'package:meta/meta.dart';
 part 'user_event.dart';
 part 'user_state.dart';
 
--- lib/bloc/user/user_event.dart
+++ lib/bloc/user/user_event.dart
@@ -2,18 +2,14 @@ part of 'user_bloc.dart';
 
 @immutable
 abstract class UsuarioEvent {}
-
 class ActivateUsuarioEvent extends UsuarioEvent {
   final Usuario usuario;
-
   ActivateUsuarioEvent(this.usuario);
 }
-
 class CambiarEdadEvent extends UsuarioEvent {
   final int edad;
   CambiarEdadEvent(this.edad);
 }
-
 class AgregarProfesionEvent extends UsuarioEvent {
   final String profesion;
   AgregarProfesionEvent(this.profesion);
